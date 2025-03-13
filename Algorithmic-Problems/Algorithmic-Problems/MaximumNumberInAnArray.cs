22c6d901a9d6cdb273e0155801852088aa669b09
rahiltariq07 <rahiltariq07@gmail.com>
2025-03-13 15:23:03 +0530

    Find the Maximum Number in an Array

--- Algorithmic-Problems/Algorithmic-Problems/MaximumNumberInAnArray.cs
+++ Algorithmic-Problems/Algorithmic-Problems/MaximumNumberInAnArray.cs
@@ -1,27 +1,27 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Algorithmic_Problems
-//{
-//    internal class MaximumNumberInAnArray
-//    {
-//        static void Main()
-//        {
-//            int[] nums = { 2, 4, 3, 7, 9 };
+namespace Algorithmic_Problems
+{
+    internal class MaximumNumberInAnArray
+    {
+        static void Main()
+        {
+            int[] nums = { 2, 4, 3, 7, 9 };
 
-//            int max = nums[0];
+            int max = nums[0];
 
-//            for (int i = 1; i < nums.Length; i++)
-//            {
-//                if (nums[i] > max)
-//                {
-//                    max = nums[i];
-//                }
-//            }
-//            Console.WriteLine("Max number is " + max);
-//        }
-//    }
-//}
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                }
+            }
+            Console.WriteLine("Max number is " + max);
+        }
+    }
+}
