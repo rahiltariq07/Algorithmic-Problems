8f873c3a836dc029132c5fae22f0fe0bd3b1f45e
rahiltariq07 <rahiltariq07@gmail.com>
2025-03-13 15:38:03 +0530

    Generate Fibonacci Sequence up to N Terms

---
+++ Algorithmic-Problems/Algorithmic-Problems/FibonacciSequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmic_Problems
+{
+    internal class FibonacciSequence
+    {
+        static void Main()
+        {
+            Console.Write("Enter the number of terms for Fibonacci sequence: ");
+            int terms = int.Parse(Console.ReadLine());
+
+            int a = 0, b = 1;
+
+            Console.WriteLine("Fibonacci Sequence:");
+            for (int i = 0; i < terms; i++)
+            {
+                Console.Write(a + " ");
+
+                int next = a + b;
+                a = b;
+                b = next;
+            }
+        }
+    }
+}
