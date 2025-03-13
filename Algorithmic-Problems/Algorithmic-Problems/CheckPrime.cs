6cde1b52829e7d852bd51823f926f0de9e88e605
rahiltariq07 <rahiltariq07@gmail.com>
2025-03-13 15:21:47 +0530

    Calculate the Factorial of a Number

--- Algorithmic-Problems/Algorithmic-Problems/CheckPrime.cs
+++ Algorithmic-Problems/Algorithmic-Problems/CheckPrime.cs
@@ -1,38 +1,38 @@
-﻿using System;
+﻿//using System;
 
-namespace Algorithmic_Problems
-{
-    internal class CheckPrime
-    {
-        static void Main()
-        {
-            Console.Write("Enter a number: ");
-            int num = int.Parse(Console.ReadLine());
+//namespace Algorithmic_Problems
+//{
+//    internal class CheckPrime
+//    {
+//        static void Main()
+//        {
+//            Console.Write("Enter a number: ");
+//            int num = int.Parse(Console.ReadLine());
 
-            if (num <= 1)
-            {
-                Console.WriteLine(num + " (not prime)");
-                return;
-            }
+//            if (num <= 1)
+//            {
+//                Console.WriteLine(num + " (not prime)");
+//                return;
+//            }
 
-            bool isPrime = true;
-            for (int i = 2; i <= Math.Sqrt(num); i++)
-            {
-                if (num % i == 0)
-                {
-                    isPrime = false;
-                    break;
-                }
-            }
+//            bool isPrime = true;
+//            for (int i = 2; i <= Math.Sqrt(num); i++)
+//            {
+//                if (num % i == 0)
+//                {
+//                    isPrime = false;
+//                    break;
+//                }
+//            }
 
-            if (isPrime)
-            {
-                Console.WriteLine(num + " (prime)");
-            }
-            else
-            {
-                Console.WriteLine(num + " (not prime)");
-            }
-        }
-    }
-}
+//            if (isPrime)
+//            {
+//                Console.WriteLine(num + " (prime)");
+//            }
+//            else
+//            {
+//                Console.WriteLine(num + " (not prime)");
+//            }
+//        }
+//    }
+//}
---
+++ Algorithmic-Problems/Algorithmic-Problems/Factorial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmic_Problems
+{
+    internal class Factorial
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int factorial = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
