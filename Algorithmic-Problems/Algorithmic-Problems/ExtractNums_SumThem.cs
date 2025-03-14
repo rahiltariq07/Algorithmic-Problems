8476f13e5ca21e347162a8b8364462ca9e022f6a
rahiltariq07 <rahiltariq07@gmail.com>
2025-03-14 15:11:26 +0530

    Extract Numbers from a String and Sum Them

---
+++ Algorithmic-Problems/Algorithmic-Problems/ExtractNums_SumThem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmic_Problems
+{
+    internal class ExtractNums_SumThem
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a string with numbers: ");
+            string input = Console.ReadLine();
+
+            int sum = 0;
+
+            foreach (char c in input)
+            {
+                if (char.IsDigit(c))
+                {
+                    int digit = c - '0';
+                    sum += digit;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
