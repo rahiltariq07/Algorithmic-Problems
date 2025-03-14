e37d5b3ea4114223c146174fd00d56ea4e15135f
rahiltariq07 <rahiltariq07@gmail.com>
2025-03-14 15:54:21 +0530

    Reverse the Order of Words in a Sentence

---
+++ Algorithmic-Problems/Algorithmic-Problems/ReverseOrderOfWords.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmic_Problems
+{
+    internal class ReverseOrderOfWords
+    {
+        static void Main()
+        {
+            Console.WriteLine("Write a sentence: ");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+
+            Array.Reverse(words);
+
+            string output = string.Join(" ", words);
+
+            Console.WriteLine(output);
+        }
+    }
+}
