d43735ca96d4aee98a25389774ae5a2963f711af
rahiltariq07 <rahiltariq07@gmail.com>
2025-03-14 14:37:09 +0530

    Count Frequency of Each Character

---
+++ Algorithmic-Problems/Algorithmic-Problems/CountCharFrequency.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmic_Problems
+{
+    internal class CountCharFrequency
+    {
+        static void Main()
+        {
+            string input = "hello world";
+
+            Dictionary<char, int> frequency = new Dictionary<char, int>();
+
+            foreach(char c in input)
+            {
+                if(c == ' ')
+                    continue;
+
+                if (frequency.ContainsKey(c))
+                {
+                    frequency[c]++;
+                }
+                else
+                {
+                    frequency[c] = 1;
+                }
+            }
+
+            foreach(var pair in frequency)
+            {
+                Console.WriteLine($"{pair.Key}{pair.Value}");
+            }
+        }
+    }
+}
