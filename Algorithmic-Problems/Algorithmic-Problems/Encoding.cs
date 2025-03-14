8aa82cfd00334eb288f79cae2d2c8c57fc7ff126
rahiltariq07 <rahiltariq07@gmail.com>
2025-03-14 15:42:06 +0530

    Basic Run-Length Encoding

---
+++ Algorithmic-Problems/Algorithmic-Problems/Encoding.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmic_Problems
+{
+    internal class Encoding
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+
+            StringBuilder output = new StringBuilder();
+            int count = 1;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] == input[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    output.Append(input[i - 1]);
+
+                    if (count > 1)
+                    {
+                        output.Append(count);
+                    }
+                    count = 1;
+                }
+            }
+
+            output.Append(input[input.Length - 1]);
+
+            if (count > 1)
+            {
+                output.Append(count);
+            }
+
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
