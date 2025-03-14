5d3ff039b1027cdaa158f3dc12d59094f6e42ede
rahiltariq07 <rahiltariq07@gmail.com>
2025-03-14 14:53:08 +0530

    Remove Specific Characters from a String

---
+++ Algorithmic-Problems/Algorithmic-Problems/RemoveSpecificChars.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmic_Problems
+{
+    internal class RemoveSpecificChars
+    {
+        static void Main()
+        {
+            string input = "Rahil Tariq";
+            string vowels = "aeiouAEIOU";
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                //if (c == ' ')
+                //    continue;
+
+                if (!vowels.Contains(c))
+                {
+                    result.Append(c);
+                }
+            }
+            Console.WriteLine("String without vowels: " + result.ToString());
+        }
+    }
+}
