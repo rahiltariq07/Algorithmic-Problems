3266a865417d1ec7f33b2b4b82b9ce33714f10bc
rahiltariq07 <rahiltariq07@gmail.com>
2025-03-15 13:37:22 +0530

    Find the Missing Number in a Sequence

---
+++ Algorithmic-Problems/Algorithmic-Problems/FindUniqueChars.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmic_Problems
+{
+    internal class FindUniqueChars
+    {
+        static void Main()
+        {
+            string input = "hello";
+
+            Dictionary<char, int> frequency = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (frequency.ContainsKey(c))
+                {
+                    frequency[c]++;
+                }
+                else
+                {
+                    frequency[c] = 1;
+                }
+            }
+
+            Console.WriteLine("Unique characters (appear only once):");
+
+            foreach (var pair in frequency)
+            {
+                if (pair.Value == 1)
+                {
+                    Console.Write(pair.Key + " ");
+                }
+            }
+        }
+    }
+}
