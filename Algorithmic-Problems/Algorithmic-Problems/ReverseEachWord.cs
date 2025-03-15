ec504b8457cce9fab72a3576f96a08936b99ac60
rahiltariq07 <rahiltariq07@gmail.com>
2025-03-15 14:57:59 +0530

    Reverse Each Word in a Sentence

---
+++ Algorithmic-Problems/Algorithmic-Problems/ReverseEachWord.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmic_Problems
+{
+    internal class ReverseEachWord
+    {
+        static void Main()
+        {
+            string input = "Rahil Tariq";
+
+            string[] words = input.Split(' ');
+
+            string reversedInput = "";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+                StringBuilder reversedWord = new StringBuilder();
+
+                for (int j = word.Length - 1; j >= 0; j--)
+                {
+                    reversedWord.Append(word[j]);
+                }
+
+                reversedInput += reversedWord.ToString() + " ";
+            }
+            Console.WriteLine(reversedInput);
+        }
+    }
+}
