5a955a3aaa0375c3510a512d2852836675fe3f1b
rahiltariq07 <rahiltariq07@gmail.com>
2025-03-17 11:36:01 +0530

    Count Vowels & Constant in a String

---
+++ Algorithmic-Problems/Algorithmic-Problems/CountVowelsAndConstants.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmic_Problems
+{
+    internal class CountVowelsAndConstants
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+
+            string vowels = "aeiouAEIOU";
+
+            int vowelsCount = 0;
+            int constsCount = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+
+                if (char.IsLetter(c))
+                {
+                    if (vowels.IndexOf(c) != -1)
+                    {
+                        vowelsCount++;
+                    }
+                    else
+                    {
+                        constsCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Vowels Count: " + vowelsCount);
+            Console.WriteLine("Constants Count: " + constsCount);
+        }
+    }
+}
