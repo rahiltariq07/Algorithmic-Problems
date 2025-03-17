488d96e3cd1d2f76181bc7356dd9df332786f86a
rahiltariq07 <rahiltariq07@gmail.com>
2025-03-17 15:31:33 +0530

    Remove Duplicate Characters from a String

---
+++ Algorithmic-Problems/Algorithmic-Problems/RemoveDuplicateChars.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmic_Problems
+{
+    internal class RemoveDuplicateChars
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a String: ");
+            string input = Console.ReadLine();
+
+            string result = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+
+                if (result.IndexOf(c) == -1)
+                {
+                    result += c;
+                }
+            }
+
+            Console.WriteLine("String after removing duplicates: " + result);
+            Console.ReadLine();
+        }
+    }
+}
